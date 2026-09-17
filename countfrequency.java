611b75c0670c8bd1afedeb284a63714657306df9
MonishaAnandan <monishaanand697@gmail.com>
2026-09-17 06:17:38 +0530

    Add files via upload

---
+++ countfrequency.java
@@ -0,0 +1,12 @@
+package javaprogram;
+import java.util.Scanner;
+public class Countfrequency {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String n=sc.nextLine();
+		for(int i=0;i<n.length();i++) {
+			count[n.chatAt(i)]++;
+		}
+	}
+
+}
