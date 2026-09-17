9f65c68d6db9b1321177393561e325af23b04d78
MonishaAnandan <monishaanand697@gmail.com>
2026-09-17 06:05:26 +0530

    Add files via upload

---
+++ swap to numbers.java
@@ -0,0 +1,19 @@
+package javaprogram;
+import java.util.Scanner;
+public class swaptonumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num1=10;
+		int num2=20;
+		int temp;
+		temp=num1;
+		num1=num2;
+		num2=temp;
+		System.out.println("a =" +num1);
+		System.out.println("b =" +num2);
+
+
+	}
+
+}
