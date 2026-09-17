817d0354a3981ade9589598d9fe74b4e2f281984
MonishaAnandan <monishaanand697@gmail.com>
2026-09-17 06:14:43 +0530

    Add files via upload

---
+++ mergetwostring.java
@@ -0,0 +1,10 @@
+package javaprogram;
+import java.util.Scanner;
+public class Mergetwostring {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		String b=sc.nextLine();
+		System.out.print(a+b);
+}
+}
