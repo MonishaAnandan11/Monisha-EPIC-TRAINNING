c4fcb730f02bb440029d647f4ea12e43abcae518
MonishaAnandan <monishaanand697@gmail.com>
2026-09-23 13:16:59 +0530

    Add files via upload

---
+++ copyonestring.java
@@ -0,0 +1,9 @@
+package javaprogram;
+import java.util.Scanner;
+public class Copyonestring {
+	public static void main(String[] args) {
+	  Scanner sc=new Scanner(System.in);
+	  String n=sc.nextLine();
+	  System.out.print(n);
+	  }
+}
